34d33441404cafd888f72c72cfbd9e3d293bce58
Marjan02 <kevinzevalja@gmail.com>
2022-12-08 20:53:14 +0700

    Update Layout & Form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,88 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Form</title>
-    
-	<link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-  </head>
-
-  <body>
+@extends('layout')
+@section('container')
     <section class="container">
-      <header>
-        <a href="#" class="logo">Landing<span>Laptop</span></a>
-    
-        <ul class="navlist">
-          <li><a href="/">Home</a></li>
-          <li><a href="/form">Gtww</a></li>
-        </ul>
-    
-        <div class="icons">
-          <a href="#"><i class='bx bx-search-alt-2'></i></a>
-        </div>
-      </header>
-      <form action="#" class="form">
-        <div class="input-box">
-          <label>Full Name</label>
-          <input type="text" placeholder="Enter full name" required />
-        </div>
-
-        <div class="input-box">
-          <label>Purposes</label>
-          <input type="text" placeholder="Enter purposes" required />
-        </div>
-
-        <div class="column">
-          <div class="input-box">
-            <label>NIS</label>
-            <input type="number" placeholder="Enter nis number" required />
-          </div>
-
-          <div class="column">
+        <form action="#" class="form">
             <div class="input-box">
-              <label>Rayon</label>
-              <input type="text" placeholder="Enter rayon" required />
-            </div>
-            
-        <div class="teacher-box">
-          <h3>Teacher</h3>
-          <div class="teacher-option">
-            <div class="teacher">
-              <input type="radio" id="check-teacher" name="teacher" checked />
-              <label for="check-other">Fema</label>
-              <input type="radio" id="check-teacher" name="teacher" checked />
-              <label for="check-other">David</label>
-              <input type="radio" id="check-teacher" name="teacher" checked />
-              <label for="check-other">Rio</label>
-              <input type="radio" id="check-teacher" name="teacher" checked />
-              <label for="check-other">Riska</label>
+                <label>Full Name</label>
+                <input type="text" placeholder="Enter full name" required />
             </div>
 
-        <div class="input-box rombel">
-          <label>Rombel </label>
-          <div class="column">
-            <div class="select-box">
-              <select>
-                <option hidden>please select</option>
-                <option>PPLG 1</option>
-                <option>PPLG 2</option>
-                <option>PPLG 3</option>
-                <option>PPLG 4</option>
-                <option>PPLG 5</option>
-              </select>
+            <div class="input-box">
+                <label>Purposes</label>
+                <input type="text" placeholder="Enter purposes" required />
             </div>
-        <button class="btn submit">Submit</button>
-    
-      </form>
+
+            <div class="column">
+                <div class="input-box">
+                    <label>NIS</label>
+                    <input type="number" placeholder="Enter nis number" required />
+                </div>
+
+                <div class="input-box">
+                    <label>Rayon</label>
+                    <input type="text" placeholder="Enter rayon" required />
+                </div>
+            </div>
+
+            <div class="teacher-box">
+                <h3>Teacher</h3>
+                <div class="teacher-option">
+                    <div class="teacher">
+                        <input type="radio" id="check-teacher" name="teacher" checked />
+                        <label for="check-other">Fema</label>
+                        <input type="radio" id="check-teacher" name="teacher" checked />
+                        <label for="check-other">David</label>
+                        <input type="radio" id="check-teacher" name="teacher" checked />
+                        <label for="check-other">Rio</label>
+                        <input type="radio" id="check-teacher" name="teacher" checked />
+                        <label for="check-other">Riska</label>
+                    </div>
+
+                    <div class="input-box rombel">
+                        <label>Rombel </label>
+                        <div class="column">
+                            <div class="select-box">
+                                <select>
+                                    <option hidden>please select</option>
+                                    <option>PPLG 1</option>
+                                    <option>PPLG 2</option>
+                                    <option>PPLG 3</option>
+                                    <option>PPLG 4</option>
+                                    <option>PPLG 5</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn submit">Submit</button>
+            </div>
+        </form>
     </section>
-
-  </body>
-</html>
-
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,47 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-	<title>LANDING LAPTOP</title>
-</head>
-
-<body>
-	 
-	<header>
-		<a href="#" class="logo">Landing<span>Laptop</span></a>
-
-		<ul class="navlist">
-			<li><a href="/">Home</a></li>
-			<li><a href="/form">Landing</a></li>
-		</ul>
-
-		<div class="icons">
-			<a href="#"><i class='bx bx-search-alt-2'></i></a>
-		</div>
-	</header>
-
-	<section class="main-header">
-		<div class="main-text">
-			<h5>Welcome!</h5>
-			<h1>Landing Laptop<br> <span>For</span> PPLG</h1>
+@extends('layout')
+@section('container')
+    <section class="main-header">
+        <div class="main-text">
+            <h5>Welcome!</h5>
+            <h1>Landing Laptop<br> <span>For</span> PPLG</h1>
             <br>
-				<a href="#" class="btn">BORROW</a>
-			</div>
-		</div>
-	</section>
+            <a href="#" class="btn">BORROW</a>
+        </div>
+        </div>
+    </section>
 
-	<div class="right">
-		<a href="#" class="scroll">Scroll Down<i class='bx bx-chevrons-down' ></i></a>
-	</div>
-
-	<!----custom js link--->
-	<script type="text/javascript" src="js/script.js"></script>
-
-</body>
-</html>
+    <div class="right">
+        <a href="#" class="scroll">Scroll Down<i class='bx bx-chevrons-down'></i></a>
+    </div>
+    </section>
+@endsection
