d125e4520a19999d74f11972f447b1cd211bc39d
nazifaumar <nazifaumar@smkwikrama.sch.id>
2022-12-13 20:29:56 +0700

    pembaruan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,7 @@
             <h5>Welcome!</h5>
             <h1>Landing Laptop<br> <span>For</span> PPLG</h1>
             <br>
-            <a href="#" class="btn">BORROW</a>
         </div>
-        </div>
-    </section>
-
-    <div class="right">
-        <a href="#" class="scroll">Scroll Down<i class='bx bx-chevrons-down'></i></a>
     </div>
     </section>
 @endsection
